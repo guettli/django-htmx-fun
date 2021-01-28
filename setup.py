03b9be128ccf8d89b0ed2d1cbf05a0439852b8dc
Thomas Guettler <guettliml@thomas-guettler.de>
2021-01-28 20:27:16 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-htmx-fun"
+    version="0.0.1",
+    author="Thomas Güttler",
+    author_email="info.django-htmx-fun@thomas-guettler.de",
+    description="A small Django application to advertise the fun htmx can bring you."
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guettli/django-htmx-fun/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
